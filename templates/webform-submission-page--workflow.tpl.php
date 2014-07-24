1fc3f11052cd1df705eb0a7399393579290474cf
Patrick Dawkins <pjcdawkins@gmail.com>
2014-07-24 09:46:25 +0100

    Improved the display of workflow information on submissions (so that it's more themeable).

---
+++ templates/webform-submission-page--workflow.tpl.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @file
+ * Customize the navigation shown when editing or viewing submissions.
+ *
+ * Available variables:
+ * - $node: The node object for this webform.
+ * - $mode: Either "form" or "display". May be other modes provided by other
+ *          modules, such as "print" or "pdf".
+ * - $submission: The Webform submission array.
+ * - $submission_content: The contents of the webform submission.
+ * - $submission_navigation: The previous submission ID.
+ * - $submission_information: The next submission ID.
+ * - $submission_workflow: The submission workflow form.
+ */
+?>
+
+<?php if ($mode == 'display' || $mode == 'form'): ?>
+  <div class="clearfix">
+    <?php print $submission_actions; ?>
+    <?php print $submission_navigation; ?>
+  </div>
+<?php endif; ?>
+
+<?php print $submission_information; ?>
+
+<?php print $submission_workflow; ?>
+
+<div class="webform-submission">
+  <?php print render($submission_content); ?>
+</div>
+
+<?php if ($mode == 'display' || $mode == 'form'): ?>
+  <div class="clearfix">
+    <?php print $submission_navigation; ?>
+  </div>
+<?php endif; ?>
+
