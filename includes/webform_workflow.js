22597482a56e21c30a14f7b5850551a2ef1bb237
Patrick Dawkins <pjcdawkins@gmail.com>
2014-07-26 10:20:09 +0100

    Made sure optgroups are not coloured by JS; added comments.

--- includes/webform_workflow.js
+++ includes/webform_workflow.js
@@ -1,10 +1,20 @@
 /**
  * @file
  * JS for the Webform Workflow module.
+ *
+ * This JavaScript is attached to several forms:
+ *  - The workflow state creating/editing form, usually seen inside an Inline
+ *    Entity Form: see webform_workflow_state_form_attach().
+ *  - The workflow state change form, shown when viewing a submission: see
+ *    webform_workflow_submission_state_form().
+ *  - The exposed filters in the View that shows a list of form submissions
+ *    (webform_workflow_submissions).
  */
 (function ($, Drupal) {
   Drupal.behaviors.webform_workflow_state_colors = {
     attach: function (context, settings) {
+      // Split out the <select> element offering color options into separate
+      // colored buttons in a <ul> list.
       $('select.webform-workflow-state-color-list').once('webform-workflow-state-colors').each(function() {
         var select_element = $(this);
         var list = $('<ul>').addClass('webform-workflow-state-color-options');
@@ -32,6 +42,7 @@
           return false;
         });
       });
+      // Color in the <select> elements that offer a drop-down list of states.
       if (settings.webformWorkflow !== undefined) {
         $('select.webform-workflow-state-select').once('webform-workflow-state-colors').each(function() {
           var select = $(this),
@@ -43,13 +54,19 @@
                 select.css('background-color', bgcolor).css('color', color);
               }
             };
-          select.find('option').each(function () {
+          select.find('option,optgroup').each(function () {
             var option = $(this),
               wsid = option.attr('value');
+            // The object settings.webformWorkflow.stateColors contains a list
+            // of colors, for which each of the keys is 'state-WSID'. WSID is
+            // the numeric entity ID of the state.
             if (wsid && settings.webformWorkflow.stateColors['state-' + wsid] !== undefined) {
               option.addClass('webform-workflow-state-color-' + settings.webformWorkflow.stateColors['state-' + wsid]);
             }
             else {
+              // Make sure that coloring the <select> element in
+              // updateSelectColor() does not affect the color of the empty
+              // <option> and <optgroup> elements.
               option.addClass('webform-workflow-state-color-none');
             }
           });
